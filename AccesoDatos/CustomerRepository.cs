a7d998eff9bc330e6eb0b1cc2d9d5c20a4527c28
LeonidasVS <villafrancosibrian@gmail.com>
2024-09-16 15:13:17 -0600

    Guia #1 y #2 Dapper

---
+++ AccesoDatos/CustomerRepository.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class CustomerRepository
+    {
+        public List<Customer> obtenerTodo()
+        {
+            using (var conexion=DataBase.GetSqlConnection())
+            {
+                String SelectFrom = "";
+                SelectFrom = SelectFrom + "SELECT [CustomerID] " + "\n";
+                SelectFrom = SelectFrom + "      ,[CompanyName] " + "\n";
+                SelectFrom = SelectFrom + "      ,[ContactName] " + "\n";
+                SelectFrom = SelectFrom + "      ,[ContactTitle] " + "\n";
+                SelectFrom = SelectFrom + "      ,[Address] " + "\n";
+                SelectFrom = SelectFrom + "      ,[City] " + "\n";
+                SelectFrom = SelectFrom + "      ,[Region] " + "\n";
+                SelectFrom = SelectFrom + "      ,[PostalCode] " + "\n";
+                SelectFrom = SelectFrom + "      ,[Country] " + "\n";
+                SelectFrom = SelectFrom + "      ,[Phone] " + "\n";
+                SelectFrom = SelectFrom + "      ,[Fax] " + "\n";
+                SelectFrom = SelectFrom + "  FROM [dbo].[Customers]";
+
+                var clientes = conexion.Query<Customer>(SelectFrom).ToList();
+                return clientes;
+
+
+            }
+
+        }
+    }
+}
---
+++ DapperDemo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DapperDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        CustomerRepository customer = new CustomerRepository();
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            var customerObtendiro = customer.obtenerTodo();
+            dataGridView1.DataSource = customerObtendiro;
+        }
+    }
+}
